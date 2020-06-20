8e5e3b47a3826c7b0ec8d5cca70cec7eeaee1509
tomonakar <tomohisa.nak@gmail.com>
2020-06-20 20:13:00 +0900

    feat: gravatarの利用テストを追加

--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
@@ -21,5 +23,19 @@ func TestAuthAvatar(t *testing.T) {
 		if url != testUrl {
 			t.Error("AuthAvatar.GetAvatarURLは正しいURLを返すべき")
 		}
+	}
+}
+
+func TestGravatarAvatar(t *testing.T) {
+	var gravatarAvatar GravatarAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{"email": "MyEmailAddress@example.com"}
+	url, err := gravatarAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("GravatarAvatar.GetAvatarURLはエラーを返すべきではありません")
+	}
+	if url !=
+		"//www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346" {
+		t.Errorf("GravatarAvatar.GetAvatarURLが%sという誤った値を返しました", url)
 	}
 }
