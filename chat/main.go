5890fceba59a817aa2fd835b7997192718c425b7
tomonakar <tomohisa.nak@gmail.com>
2020-06-20 21:30:57 +0900

    feat: ファイルアップローダーを追加

--- chat/main.go
+++ chat/main.go
@@ -53,6 +53,8 @@ func main() {
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
 			Name:   "auth",
