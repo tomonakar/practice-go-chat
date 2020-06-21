a4ff4a32fb2a78193c890c99abfa8436c19bde49
tomonakar <tomohisa.nak@gmail.com>
2020-06-21 13:01:22 +0900

    feat: filesystemAvatarを使用したAvatar表示

--- chat/main.go
+++ chat/main.go
@@ -47,7 +47,8 @@ func main() {
 	)
 
 	// r := newRoom(UseAuthAvatar)
-	r := newRoom(UseGravatar)
+	// r := newRoom(UseGravatar)
+	r := newRoom(UseFileSystemAvatar)
 	r.tracer = trace.New(os.Stdout)
 
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
