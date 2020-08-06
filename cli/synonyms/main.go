8ea5c204dafbf425f3637663006fc590b914896e
tomonakar <tomohisa.nak@gmail.com>
2020-08-06 09:23:35 +0900

    feat: 類語検索

---
+++ cli/synonyms/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"cli/thesaurus"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	apiKey := os.Getenv("BHT_APIKEY")
+	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := s.Text()
+		syns, err := thesaurus.Synonyms(word)
+		if err != nil {
+			fmt.Println(syns)
+			log.Fatalf("%qの類語検索に失敗しました: %v\n", word, err)
+		}
+		if len(syns) == 0 {
+			log.Fatalf("%qに類語はありませんでした\n")
+		}
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
+}
